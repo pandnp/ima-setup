945eacdf29cb2f454e1290d1ccfa0e9a1855668f
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2014-01-28 11:52:19 -0600

    clean up old runs and remove extra spaces

--- grub-fix-mode.sh
+++ grub-fix-mode.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-if [ $1 == 1 ]; then
-OPTIONS='ima_tcb ima_appraise_tcb evm=fix ima_appraise=fix evmkey=\/etc\/keys\/evm-user.blob evmx509=\/etc\/keys\/local_x509.der'
-else
-OPTIONS='ima_tcb ima_appraise_tcb evm=fix ima_appraise=fix evmkey=\/etc\/keys\/evm-trusted.blob evmx509=\/etc\/keys\/local_x509.der'
+if [$1 == 1]; then # vm mode 
+evmpath="evmkey=\/etc\/keys\/evm-user.blob"
+else # hw mode
+evmpath="evmkey=\/etc\/keys\/evm-trusted.blob"
 fi
 
-grub2-mkconfig -o /boot/grub2/grub.cfg
+options="ima_tcb ima_appraise_tcb evm=fix ima_appraise=fix evmx509=\/etc\/keys\/local_x508.der $evmpath"
 
-sed -i "s/GRUB_CMDLINE_LINUX=\"\(.*\)\"/GRUB_CMDLINE_LINUX=\"\1 $OPTIONS\"/" /etc/default/grub
+# clean up changes from previous runs
+for i in "${options[@]}"
+do
+  :
+  sed -i "s/$i //g" /etc/default/grub
+  sed -i "s/$i//g" /etc/default/grub
+done
 
+# set new ima-appraisal options
+sed -i "s/GRUB_CMDLINE_LINUX=\"\(.*\)\"/GRUB_CMDLINE_LINUX=\"\1 $options\"/" /etc/default/grub
+
+# regenerate grub.cfg
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
 reboot
