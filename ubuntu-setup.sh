f741f5464c2a1bbcc20a8bd91b1e9ae160e25346
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2014-01-29 12:45:24 -0600

    removed generic setup and added distro specific scripts

---
+++ ubuntu-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VM=1 # set VM=1 for virtual machine, set VM=0 for hardware
+
+./install-ima-evm-utils.sh
+#./install-dracut-patches.sh
+./install-evmctl.sh
+./create-keys.sh $VM
+./mount-iversion.sh
+./refresh-initramfs.sh
+./grub-fix-mode.sh $VM
