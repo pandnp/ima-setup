78bb1a0ae81be8473e38519bcc20433d61d07f1e
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2014-02-03 23:38:06 -0600

    fix argument bug

--- ubuntu/create-keys.sh
+++ ubuntu/create-keys.sh
@@ -4,7 +4,7 @@
 cd /usr/bin/r7-labeling
 # edit evm_create_keys.sh, changing NOTPM to TPM, if you have a TPM.
 
-if [ $1 == 1 ]; then
+if [ $1 == "1" ]; then
 	sed -i 's/ TPM/ NOTPM/g' evm_create_keys.sh
 else
 	sed -i 's/ NOTPM/ TPM/g' evm_create_keys.sh
@@ -12,7 +12,7 @@ fi
 
 ./evm_create_keys.sh
 
-if [ $1 == 1 ]; then
+if [ $1 == "1" ]; then
 MASTERKEY='MULTIKERNELMODE="NO"
 MASTERKEYTYPE="user"
 MASTERKEY="/etc/keys/kmk-${MASTERKEYTYPE}.blob"'
