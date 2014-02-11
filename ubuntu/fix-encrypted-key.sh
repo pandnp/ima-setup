3234f63449e15b059bfa0fec310b125d613fe9d4
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2014-02-11 17:58:25 -0600

    unlink before re-adding key

--- ubuntu/fix-encrypted-key.sh
+++ ubuntu/fix-encrypted-key.sh
@@ -2,5 +2,6 @@
 
 keyctl add encrypted evm-key "new user:kmk-user 32" @u
 keyctl pipe `keyctl search @u encrypted evm-key` > /etc/keys/evm-user.blob
+keyctl unlink `keyctl search @u encrypted evm-key`
 keyctl add encrypted evm-key "load `cat /etc/keys/evm-user.blob`" @u
 update-initramfs -u
