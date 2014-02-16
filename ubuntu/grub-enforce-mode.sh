08a4d8d342af5c8ad48cffa125ac0f1e764d7bd6
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2014-02-15 18:48:10 -0600

    inverted logic

--- ubuntu/grub-enforce-mode.sh
+++ ubuntu/grub-enforce-mode.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-vm="evmkey=\/etc\/keys\/evm-user.blob"
+notpm="evmkey=\/etc\/keys\/evm-user.blob"
 tpm="evmkey=\/etc\/keys\/evm-trusted.blob"
 
 if [ $1 -eq "1" ]; then # vm mode 
-evmpath=$vm
-else # tpm mode
 evmpath=$tpm
+else # tpm mode
+evmpath=$notpm
 fi
 
 options="ima_tcb ima_appraise_tcb evmx509=\/etc\/keys\/local_x509.der $evmpath"
 
-remove=("ima_tcb" "ima_appraise_tcb" "evm=fix" "ima_appraise=fix" "evmx509=\/etc\/keys\/local_x509.der" "$vm" "$tpm")
+remove=("ima_tcb" "ima_appraise_tcb" "evm=fix" "ima_appraise=fix" "evmx509=\/etc\/keys\/local_x509.der" "$notpm" "$tpm")
 
 # clean up changes from previous runs
 for i in "${remove[@]}"
