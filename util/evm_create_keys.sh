0285eb0f55c9811da883bfe3c7543c7e912aba40
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2014-02-16 08:55:54 -0600

    fixed conditions

--- util/evm_create_keys.sh
+++ util/evm_create_keys.sh
@@ -3,15 +3,15 @@
 # Create symmetric and asymmetric keys for the integrity subsystem.
 # The symmetric key is used to calculate the EVM HMAC.  The asymmetric
 # key is used to sign/verify immutable file signatures.
-
+set -x
 # Create symmetric key used for the EVM HMAC
 PWD=`pwd`
 TPM=$1
 
 su -c 'mkdir -p /etc/keys'
-if [ $TPM -eq "tpm" ]; then
+if [ "$TPM" == "tpm" ]; then
   su - -c "$PWD/evm_create_symkey.sh TPM"
-elif [ $TPM -eq "notpm" ]; then
+elif [ "$TPM" == "notpm" ]; then
   su - -c "$PWD/evm_create_symkey.sh NOTPM"
 fi
 
