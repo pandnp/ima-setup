3933d1336ac366df1897048964fc044bb1137c93
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2014-02-15 19:15:59 -0600

    create initramfs scritps on the fly

--- ubuntu/mkinitramfs.sh
+++ ubuntu/mkinitramfs.sh
@@ -1,15 +1,76 @@
 #!/bin/sh
 
-# $1 = VM
-if [ $1 -eq "1" ]; then
-	cp notpm-hooks /usr/share/initramfs-tools/hooks/ima
-	cp notpm-premount /usr/share/initramfs-tools/scripts/init-premount/ima
+# $1 = TPM
+if [ $1 == "1" ];
+  kmk='/etc/keys/kmk-trusted.blob'
+  evm='/etc/keys/evm-trusted.blob'
+  load_kmk='keyctl add trusted kmk-trusted "load `cat /etc/keys/kmk-trusted.blob`" @u > /dev/null'
+  load_evm='keyctl add encrypted evm-key "load `cat /etc/keys/evm-trusted.blob`" @u > /dev/null'
 else
-	cp tpm-hooks /usr/share/initramfs-tools/hooks/ima
-	cp tpm-premount /usr/share/initramfs-tools/scripts/init-premount/ima
+  kmk='/etc/keys/kmk-user.blob'
+  evm='/etc/keys/evm-user.blob'
+  load_kmk='keyctl add user kmk-user "`cat /etc/keys/kmk-user.blob`" @u > /dev/null'
+  load_evm='keyctl add encrypted evm-key "load `cat /etc/keys/evm-user.blob`" @u > /dev/null'
 fi
 
+ima_hooks='#!/bin/sh
+PREREQ=""
+prereqs()
+{
+     echo "$PREREQ"
+}
 
-cd /boot
-mkinitramfs -k -o initrd.img-`uname -r` `uname -r`
-#update-initramfs -u
+case $1 in
+prereqs)
+     prereqs
+     exit 0
+     ;;
+esac
+
+# Begin real processing below this line
+. /usr/share/initramfs-tools/hook-functions
+copy_exec /sys/kernel/security/evm
+copy_exec /usr/local/bin/evmctl /sbin
+copy_exec /bin/keyctl /sbin
+copy_exec /etc/keys/local_x509.der
+copy_exec /etc/keys/private/local_priv.pem'
+
+ima_hooks="$ima_hooks
+copy_exec $kmk
+copy_exec $evm"
+
+ima_premount='#!/bin/sh
+PREREQ=""
+prereqs()
+{
+     echo "$PREREQ"
+}
+
+case $1 in
+prereqs)
+     prereqs
+     exit 0
+     ;;
+esac
+
+ima_id=`/sbin/keyctl newring _ima @u`
+evm_id=`/sbin/keyctl newring _evm @u`
+for PUBKEY in /etc/keys/*.der; do
+    evmctl -x import $PUBKEY $ima_id > /dev/null
+    evmctl -x import $PUBKEY $evm_id > /dev/null
+done'
+
+ima_premount="$ima_premount
+$load_kmk
+$load_evm"
+
+#enable EVM
+mount -n -t securityfs securityfs /sys/kernel/security
+echo "1" > /sys/kernel/security/evm
+
+sleep 3"
+
+echo $ima_hooks > /usr/share/initramfs-tools/hooks/ima
+echo $ima_premount > /usr/share/initramfs-tools/scripts/init-premount/ima
+
+update-initramfs -u
