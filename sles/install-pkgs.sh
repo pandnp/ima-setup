36647f25fb0d7419c57bf230cf2084127a4f6074
root <root@rainicorn.austin.ibm.com>
2014-07-09 14:32:09 -0500

    fix openssl lib package name

--- sles/install-pkgs.sh
+++ sles/install-pkgs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo zypper install automake autoconf libtool libssl-devel libattr-devel gcc keyutils-devel lldpad libcap1 biosdevname libcap-progs fcoe-utils
+sudo zypper install automake autoconf libtool libopenssl-devel libattr-devel gcc keyutils-devel lldpad libcap1 biosdevname libcap-progs fcoe-utils
